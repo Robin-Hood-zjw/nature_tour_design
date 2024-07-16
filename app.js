4a2ef78462d50500d8b04fbc48fd979794694514
Robin-Hood-zjw <jiawenz@umich.edu>
2024-07-15 20:36:55 -0400

    0.3_API_setups

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.status().json({});
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
