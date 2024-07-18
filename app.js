291cbc6d8787f8de9cf0b27c5daa8f0d641d0e04
Robin-Hood-zjw <jiawenz@umich.edu>
2024-07-18 15:39:04 -0400

    1.4.using_3rd-party_middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const morgan = require('morgan');
 const express = require('express');
-const { create } = require('domain');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json());
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
