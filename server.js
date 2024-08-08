8e01c47c79d310a9a7b2789a0513dc1222cfcf7e
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-07 23:55:54 -0700

    3.2.1.connect_App_with_Database

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then((con) => {
+  // console.log(con.connections);
+  console.log('DB connection successful');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
