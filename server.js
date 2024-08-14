02be85de5049570270328f7c6f5eacd8fd351d2b
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-13 17:30:59 -0700

    3.3.2.create_Tour_Model

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ mongoose.connect(DB).then((con) => {
 });
 
 const tourSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  rating: Number,
-  price: Number,
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price.'] },
 });
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
