2ab2b4fcd2fea8f9387ac9ca0299b91c59fd56a7
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-17 15:00:30 -0700

    3.11.2.importing_dev_data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -7,21 +7,43 @@ const Tour = require('../../models/tourModel');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  '<password>',
   process.env.DATABASE_PASSWORD,
 );
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 // read JSON file
-const tours = fs.readFileSync('tours-simple.json', 'utf-8');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
 
 // import data into database
+// CMD: node dev-data/data/import-dev-data.js --import
 const importData = async () => {
   try {
-    console.log('Data successfully loaded!');
     await Tour.create(tours);
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
+  process.exit();
 };
+
+// delete all data from the database
+// CMD: node dev-data/data/import-dev-data.js --delete
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
