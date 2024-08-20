aa8c8d0a6b4dc486d1f534c8ae770efa0325da93
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-20 00:54:22 -0700

    3.25.1.validation:built_in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name.'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name cannot have over 40 characters.'],
+      minlength: [10, 'A tour name cannot have fewer than 10 characters.'],
     },
     slug: String,
     duration: {
@@ -21,8 +23,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult.',
+      },
     },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0.'],
+      max: [5, 'Rating must be below 5.0.'],
+    },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
     priceDiscount: Number,
@@ -64,14 +75,12 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  console.log(docs);
   next();
 });
 
 // aggregation middleware
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline());
   next();
 });
 
