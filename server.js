5dc9c2334c183a306c680b4afde8f5dcf9e82b86
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-21 16:01:53 -0700

    4.10.1.unhandled_rejections

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const { Server } = require('http');
 
 dotenv.config({ path: './config.env' });
 
@@ -8,12 +9,21 @@ const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD,
 );
-mongoose.connect(DB).then((con) => {
-  // console.log(con.connections);
-  console.log('DB connection successful');
-});
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => console.log('Error'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  Server.close(() => {
+    process.exit(1);
+  });
 });
