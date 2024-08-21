b66f1ef9e45e83a66ac9880bb81c81894d009017
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-21 16:46:07 -0700

    5.1.1.modeling_user_schema

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: [true, 'Please tell us your name.'] },
+    email: {
+      type: string,
+      unique: true,
+      lowercase: true,
+      required: [true, 'Please provide your email.'],
+      validate: [validator.isEmail, 'Please provide a valid email.'],
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      required: [true, 'Please provide a password.'],
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password.'],
+    },
+    photo: String,
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
