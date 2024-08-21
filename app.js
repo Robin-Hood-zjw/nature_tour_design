66dfc015dd23aa8bac6b247c74b4f4b2ffe42d14
Robin-Hood-zjw <jiawenz@umich.edu>
2024-08-20 21:57:17 -0700

    4.3.1.refactoring&better_errors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const morgan = require('morgan');
 const express = require('express');
 
+const AppError = require('./utils/appError');
 const tourRouter = require(`./routes/tourRoutes`);
 const userRouter = require(`./routes/userRoutes`);
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
@@ -23,18 +25,9 @@ app.all('*', (req, res, next) => {
   //   status: 'fail',
   //   message: `Can't find ${req.originalUrl} on this server.`,
   // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
