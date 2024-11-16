081a16521be3e59a4aa8ad1cbad5e7244a9a9d77
Robin-Hood-zjw <jiawenz@umich.edu>
2024-11-16 14:01:28 -0800

    5.6.5.sending_emails_with_nodemailer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 
-router.patch('/resetPassword/:id', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 router
   .route('/')
