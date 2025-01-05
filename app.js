7254f833971a9b32cfce3bb1c174b6e2ba502233
Robin-Hood-zjw <jiawenz@umich.edu>
2025-01-05 14:59:43 -0800

    5.13.2.implementing_rate_limiting

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const morgan = require('morgan');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
+// eslint-disable-next-line import/newline-after-import
 const AppError = require('./utils/appError');
 const tourRouter = require(`./routes/tourRoutes`);
 const userRouter = require(`./routes/userRoutes`);
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+// global middleware
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try agian in an hour!',
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
