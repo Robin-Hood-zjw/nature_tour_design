9bab62e9ed32b0ef485701fe2bcac4a5bd7101ae
Robin-Hood-zjw <jiawenz@umich.edu>
2025-01-10 15:03:56 -0800

    6.1.2.modelling_locations(geospatial_data)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -14,9 +14,7 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 // read JSON file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // import data into database
 // CMD: node dev-data/data/import-dev-data.js --import
