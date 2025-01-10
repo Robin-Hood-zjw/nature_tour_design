6418d24d2730c0ffe312f11dca30c53a1f22b62d
Robin-Hood-zjw <jiawenz@umich.edu>
2025-01-10 15:58:37 -0800

    6.3.1.modelling_your:child_referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -75,7 +75,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } },
 );
