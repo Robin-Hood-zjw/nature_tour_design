53a405931affc7957751f666953ef7bd6c1b9e1f
Robin-Hood-zjw <jiawenz@umich.edu>
2025-01-19 18:02:38 -0800

    6.10.1.nested_routes_with_Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
+const reviewRouter = require('./reviewRoutes');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -29,12 +31,12 @@ router
     tourController.deleteTour,
   );
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
-  );
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview,
+//   );
 
 module.exports = router;
