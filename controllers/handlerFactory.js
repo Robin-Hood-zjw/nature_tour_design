5a9158a91b577b3eaf02ceec596c5882bb855f03
Robin-Hood-zjw <jiawenz@umich.edu>
2025-01-24 12:18:29 -0800

    6.13.1.building_handler_function:update&create

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -2,6 +2,25 @@ const catchAsync = require('../utils/catchAsync');
 
 const AppError = require('../utils/appError');
 
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const newDoc = await Model.create(req.body);
+
+    res.status(201).json({ status: 'success', data: { tour: newDoc } });
+  });
+
+exports.uodateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) return next(new AppError('No document found with that ID', 404));
+
+    res.status(200).json({ status: 'success', data: { doc } });
+  });
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
