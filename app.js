f4c66c5e8500064f80b288627789f54070b0fc1d
Robin-Hood-zjw <jiawenz@umich.edu>
2025-03-06 12:48:41 -0800

    7.2.1.Pug_initializations

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', { tour: 'The Forest Hiker', user: 'jiawen' });
 });
 
 app.use('/api/v1/tours', tourRouter);
