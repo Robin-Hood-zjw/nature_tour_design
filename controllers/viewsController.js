113cb2e586895753bac1a5794b145798d0a18b2b
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-03 16:45:20 -0700

    7.7.2.building_tour_overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -3,7 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
-  res.status(200).render('overview', { title: 'All tours', tours: tours });
+  res.status(200).render('overview', { title: 'All tours', tours });
 });
 
 exports.getTour = catchAsync(async (req, res) => {
