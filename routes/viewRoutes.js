347e3eab215e2433b9efd7d5769db5147e78e66a
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-03 12:42:11 -0700

    7.6.2.setting_up_the_project_structure

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,10 @@
 const express = require('express');
 
-const viewsController = require('../controllers/viewsController')
+const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', { tour: 'The Forest Hiker', user: 'jiawen' });
-});
-
 router.get('/overview', viewsController.getOverview);
-
 router.get('/tour', viewsController.getTour);
 
 module.exports = router;
