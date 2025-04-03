95b1c80e005b07f681d11ee8ba0aad2d855f1ec0
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-03 12:19:02 -0700

    7.5.1.extending_base_templates_with_blocks

--- app.js
+++ app.js
@@ -72,6 +72,14 @@ app.get('/', (req, res) => {
   res.status(200).render('base', { tour: 'The Forest Hiker', user: 'jiawen' });
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', { title: 'All tours' });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', { title: 'The Forest Tour' });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
