68c76d085afd7edff0a6d113f3a3f4be76692781
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-11 16:46:08 -0700

    7.9.3.include_maps_with_Mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,4 +1,10 @@
 /* eslint-disable */
-console.log('Hello from the client side.');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiamlhd2VueiIsImEiOiJjbTlkZGYwZm4wY285MmpvZDVucXdtbm5xIn0.ZYXY9nG0vvqtUqVO_RZ2Pg';
+
+var map = new mapboxgl.Map({
+    container:'map',
+    style: 'mapbox://styles/jiawenz/cm9ddum5f009p01spg3wpena4'
+});
