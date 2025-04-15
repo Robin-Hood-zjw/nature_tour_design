2da31a62104e88e0eb59d8d555b87eb7d93e7cc8
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-15 16:31:23 -0700

    7.11.1.logging_in_users_with_API

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -6,24 +6,28 @@ exports.getOverview = catchAsync(async (req, res) => {
   res.status(200).render('overview', { title: 'All tours', tours });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user',
+    fields: 'reviews rating user',
   });
-
   const CSP =
     "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;";
 
   res
     .status(200)
     .set('Content-Security-Policy', CSP)
-    .render('tour', {
-      title: `${tour.name} Tour`,
-      tour,
-    });
+    .render('tour', { title: `${tour.name}`, tour });
 });
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', { title: 'Log into your account' });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "connect-src 'self' https://cdnjs.cloudflare.com",
+    )
+    .render('login', {
+      title: 'User Login',
+    });
 };
--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,25 @@
 /* eslint-disable */
 
-const login = (email, password) => {
-    alert(email, password);
-};
+const login = async (email, password) => {
+    alert(`${email} / ${password}`);
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = docuemnt.getElementById('email').value;
-  const password = docuemnt.getElementById('password').value;
-  login(email, password);
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:3000/api/v1/users/login',
+            data: { email, password}
+        });
+    
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
 });
