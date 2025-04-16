c96d1ace9eb6baff8dfc02ea3b60b0a16c1c5ca4
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-15 21:14:30 -0700

    7.11.6.logging_in_users_with_API

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set security http headers
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,22 +11,22 @@ exports.getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'reviews rating user',
   });
-  const CSP =
-    "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;";
+  // const CSP =
+  //   "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;";
 
   res
     .status(200)
-    .set('Content-Security-Policy', CSP)
+    // .set('Content-Security-Policy', CSP)
     .render('tour', { title: `${tour.name}`, tour });
 });
 
 exports.getLoginForm = (req, res) => {
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "connect-src 'self' https://cdnjs.cloudflare.com",
-    )
+    // .set(
+    //   'Content-Security-Policy',
+    //   "connect-src 'self' https://cdnjs.cloudflare.com",
+    // )
     .render('login', {
       title: 'User Login',
     });
