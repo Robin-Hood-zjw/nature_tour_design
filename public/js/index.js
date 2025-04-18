eb4027198a71227fb74c90721dfce72f985fb7f4
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-18 15:08:40 -0700

    7.11.19.logging_in_users_with_API

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,45 @@
 /* eslint-disable */
+import '@babel/polyfill';
 
-console.log('hello from the parcel');
+import { displayMap } from './mapbox';
+import { login, logout } from './login';
 
-const login = async (email, password) => {
-    try {
-        const res = await axios({
-            method: 'POST',
-            url: 'http://127.0.0.1:3000/api/v1/users/login',
-            data: { email, password }
-        });
+// console.log('hello from the parcel');
+
+// const login = async (email, password) => {
+//     try {
+//         const res = await axios({
+//             method: 'POST',
+//             url: 'http://127.0.0.1:3000/api/v1/users/login',
+//             data: { email, password }
+//         });
         
-        if (res.data.status === 'success') {
-            alert('Logged in successfully.')
-            window.setTimeout(() => location.assign('/'), 1500);
-        }
-    } catch (err) {
-        alert(err);
-    }
-};
+//         if (res.data.status === 'success') {
+//             alert('Logged in successfully.')
+//             window.setTimeout(() => location.assign('/'), 1500);
+//         }
+//     } catch (err) {
+//         alert(err);
+//     }
+// };
 
-const logout = async () => {
-    try {
-        const res = await axios({
-            method: 'GET',
-            url: 'http://127.0.0.1:3000/api/v1/users/logout'
-        });
+// const logout = async () => {
+//     try {
+//         const res = await axios({
+//             method: 'GET',
+//             url: 'http://127.0.0.1:3000/api/v1/users/logout'
+//         });
 
-        if ((res.data.status = 'success')) location.reload(true);
-    } catch (err) {
-        alert(err.response.data.message);
-    }
-};
+//         if ((res.data.status = 'success')) location.reload(true);
+//     } catch (err) {
+//         alert(err.response.data.message);
+//     }
+// };
 
 const loginForm = document.querySelector('.form--login');
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+displayMap(locations);
 
 if (loginForm) {
     loginForm.addEventListener('submit', e => {
@@ -40,6 +47,6 @@ if (loginForm) {
     
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
-        access.login(email, password);
+        login(email, password);
     });
 }
---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+exports.login = async (email, password) => {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:3000/api/v1/users/login',
+            data: { email, password }
+        });
+        
+        if (res.data.status === 'success') {
+            alert('Logged in successfully.')
+            window.setTimeout(() => location.assign('/'), 1500);
+        }
+    } catch (err) {
+        alert(err);
+    }
+};
+
+exports.logout = async () => {
+    try {
+        const res = await axios({
+            method: 'GET',
+            url: 'http://127.0.0.1:3000/api/v1/users/logout'
+        });
+
+        if ((res.data.status = 'success')) location.reload(true);
+    } catch (err) {
+        alert(err.response.data.message);
+    }
+};
