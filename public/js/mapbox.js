30eef8d0b7f1dcb15d7ba336c931c0f65d664622
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-23 15:56:06 -0700

    7.11.25.logging_in_users_with_API

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -33,34 +33,3 @@ export const displayMap = (locations) => {
         map.fitBounds(bounds, { top:200, bottom:150, left:100, right:100 });
     });
 };
-
-// mapboxgl.accessToken = 'pk.eyJ1Ijoiamlhd2VueiIsImEiOiJjbTlkZGYwZm4wY285MmpvZDVucXdtbm5xIn0.ZYXY9nG0vvqtUqVO_RZ2Pg';
-
-// var map = new mapboxgl.Map({
-//     container:'map',
-//     style: 'mapbox://styles/jiawenz/cm9ddum5f009p01spg3wpena4',
-//     scrollZoom: false,
-//     // center: [],
-//     // zoom: 10,
-//     // interactive: false,
-// });
-
-// const bounds = new mapboxgl.LngLatBounds();
-
-// locations.forEach(loc => {
-//     // create a marker
-//     const el = document.createElement('div');
-//     el.className = 'marker';
-
-//     new mapboxgl.Marker({ element: el, anchor: 'bottom'})
-//         .setLngLat(loc.coordinates)
-//         .addTo(map);
-
-//     new mapboxgl.Popup({ offset: 30 })
-//         .setLngLat(loc.coordinates)
-//         .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-//         .addTo(map);
-
-//     bounds.extend(loc.coordinates);
-//     map.fitBounds(bounds, { top:200, bottom:150, left:100, right:100 });
-// });
