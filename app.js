c06353f4529715c4709cd8228aa8f4fd07c9cb2e
Robin-Hood-zjw <jiawenz@umich.edu>
2025-04-28 11:24:51 -0700

    7.15.1.update_user_data

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use('/api', limiter);
 
 // body parser - read data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true,  }));
 app.use(cookieParser());
 
 // data sanitization against NoSQL query injection
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,7 @@
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const AppError = require('../utils/appError');
+
 const catchAsync = require('../utils/catchAsync');
 
 const policy = "default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;";
@@ -36,7 +38,20 @@ exports.getLoginForm = (req, res) => {
 };
 
 exports.getAccount = (req, res) => {
-  res.status(200).render('account', {
-    title: 'Your account'
-  });
+  res.status(200).render('account', { title: 'Your account' });
 };
+
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  // console.log('UPDATE BODY', req.body);
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { name: req.body.name, email: req.body.email},
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
+  });
+});
