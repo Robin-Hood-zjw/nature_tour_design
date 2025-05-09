940583c27dc49ef6044621307e7e6018f1141b94
Robin-Hood-zjw <jiawenz@umich.edu>
2025-05-09 15:55:13 -0700

    8.8.1.building_email_handler

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,49 @@
+const pug = require('pug');
 const nodemailer = require('nodemailer');
+const htmlToText = require('html-to-text');
+
+// new Email(user, url).sendWelcome();
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Jiawen Zhang <${process.env.EMAIL_FROM}>`;
+  };
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') return 1;
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  };
+
+  send(template, subject) {
+    // render the HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`);
+
+    // define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject, html,
+      text: options.message,
+    };
+
+    // create a transport and email
+  };
+
+  sendWelcome() {
+    this.send('Welcome', 'Welcome to the Natours family!');
+  }
+}
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
