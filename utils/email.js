8a6d6b7441712fcae85e7831e90043fb5c3a9dc7
Robin-Hood-zjw <jiawenz@umich.edu>
2025-05-09 16:19:53 -0700

    8.8.2.building_email_handler

--- utils/email.js
+++ utils/email.js
@@ -2,8 +2,6 @@ const pug = require('pug');
 const nodemailer = require('nodemailer');
 const htmlToText = require('html-to-text');
 
-// new Email(user, url).sendWelcome();
-
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
@@ -12,7 +10,7 @@ module.exports = class Email {
     this.from = `Jiawen Zhang <${process.env.EMAIL_FROM}>`;
   };
 
-  createTransport() {
+  newTransport() {
     if (process.env.NODE_ENV === 'production') return 1;
 
     return nodemailer.createTransport({
@@ -25,44 +23,48 @@ module.exports = class Email {
     });
   };
 
-  send(template, subject) {
+  async send(template, subject) {
     // render the HTML based on a pug template
-    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`);
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      { firstName: this.firstName, url: this.url, subject }
+    );
 
     // define email options
     const mailOptions = {
       from: this.from,
       to: this.to,
       subject, html,
-      text: options.message,
+      text: htmlToText.fromString(html),
     };
 
     // create a transport and email
+    await this.newTransport().sendMail(mailOptions);
   };
 
-  sendWelcome() {
-    this.send('Welcome', 'Welcome to the Natours family!');
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to the Natours family!');
   }
 }
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
 
-  const mailOptions = {
-    from: 'Jiawen Zhang <jiawenz>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+//   const mailOptions = {
+//     from: 'Jiawen Zhang <jiawenz>',
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
 
-  await transporter.sendMail(mailOptions);
-};
+//   await transporter.sendMail(mailOptions);
+// };
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
