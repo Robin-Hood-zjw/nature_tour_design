dd5519441f9b71bb2ddd882f8e118f5adaa8ca7b
Robin-Hood-zjw <jiawenz@umich.edu>
2025-05-20 15:59:00 -0700

    8.12.3.integating_Stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,6 @@
-const stripe = require('stripe');
+const Stripe = require('stripe');
 
 const Tour = require('../models/tourModel');
-
 const AppError = require('../utils/appError');
 
 const factory = require('./handlerFactory');
@@ -9,7 +8,33 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getCheckout = catchAsync(async (req, res, next) => {
     // get the currently booked tous
+    const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
     const tour = await Tour.findById(req.params.tourId);
+
     // create checkout sessions
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        success_url: `${req.protocol}://${req.get('host')}/`,
+        cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+        customer_email: req.user.email,
+        client_reference_id: req.params.tourId,
+        line_items: [
+            {
+                quantity: 1,
+                price_data: {
+                currency: 'inr',
+                unit_amount: tour.price,
+                product_data: {
+                    name: `${tour.name} Tour`,
+                    description: tour.summary,
+                    images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                },
+                },
+            },
+        ],
+    });
+
     // create sessions as response
+    res.status(200).json({ status: 'success', session });
 });
